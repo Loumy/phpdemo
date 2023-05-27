a1b4c0a88094e71970e7071924c9abb7661a4b9e
Cyril Lopez <cylopez@redhat.com>
2023-05-27 11:47:23 +0200

    fix la date
    Signed-off-by: Cyril Lopez <cylopez@redhat.com>

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h2>top ce php ".phpversion()."</h2>";
-echo "<p>et dynamique la preuve il est :". date(). " dans mon container " . gethostname();
+echo "<p>et dynamique la preuve il est :". date('Y-m-d H:i:s'). " dans mon container " . gethostname();
 ?>
